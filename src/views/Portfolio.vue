89e8138337afc3021ccf8429cf5170220d6e1a28
dhershman1 <dustinh17@gmail.com>
2024-01-21 15:02:10 -0500

    Update to portfolio

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,24 +1,31 @@
 <template>
   <section class="portfolio">
     <h1>Portfolio</h1>
-    <div class="cards">
+    <div class="card-container">
       <card
         v-for="(project, i) in projects"
         :key="i"
         :go-to-link="project.link"
+        :icon="project.icon"
       >
         <template #main>
           <div class="project-name">
             <h2>{{ project.name }}</h2>
-          </div>
-          <div>
             <p class="summary">
               {{ project.summary }}
             </p>
           </div>
         </template>
         <template #text>
-          <p>Maybe a badge or 2?</p>
+          <a
+            v-for="(b, j) in project.badges"
+            :key="j"
+            :href="b.link"
+            target="_blank"
+            class="badge"
+          >
+            <img :src="b.badge">
+          </a>
         </template>
       </card>
     </div>
@@ -33,15 +40,40 @@ const projects = computed(() => {
   return [
     {
       name: 'Kyanite',
-      type: 'Library',
-      summary: 'A fully functional library of functions for use in any app',
-      link: 'https://github.com/dhershman1/kyanite'
+      summary: 'A light weight functional JavaScript library that only depends on itself',
+      link: 'https://github.com/dhershman1/kyanite',
+      icon: 'github',
+      badges: [{
+        badge: 'https://img.shields.io/badge/Kyanite-Documentation-blue?style=flat-square',
+        link: 'https://kyanite.dusty.codes/'
+      }, {
+        badge: 'https://img.shields.io/npm/dm/kyanite.svg?style=flat-square',
+        link: 'https://www.npmjs.com/package/kyanite'
+      }]
+    }, {
+      name: 'Vue Debounce',
+      summary: 'A simple to use directive for debounce solutions',
+      link: 'https://github.com/dhershman1/vue-debounce',
+      icon: 'github',
+      badges: [{
+        badge: 'https://img.shields.io/github/stars/dhershman1/vue-debounce?style=flat-square',
+        link: 'https://github.com/dhershman1/vue-debounce'
+      }, {
+        badge: 'https://img.shields.io/npm/dm/vue-debounce.svg?style=flat-square',
+        link: 'https://www.npmjs.com/package/vue-debounce'
+      }]
     }
   ]
 })
 </script>
 
 <style scoped>
+.badge {
+  padding-right: 0.5rem;
+}
+.badge:last-child {
+  padding-right: 0;
+}
 .cards {
   margin-top: 1rem;
 }
