b9c438130f05c34e6a7658ce4c9df35898afe3f0
dhershman1 <dustinh17@gmail.com>
2024-01-21 11:30:05 -0500

    Started portfolio design

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,5 +1,51 @@
 <template>
-  <section>
+  <section class="portfolio">
     <h1>Portfolio</h1>
+    <div class="cards">
+      <card
+        v-for="(project, i) in projects"
+        :key="i"
+        :go-to-link="project.link"
+      >
+        <template #main>
+          <div class="project-name">
+            <h2>{{ project.name }}</h2>
+          </div>
+          <div>
+            <p class="summary">
+              {{ project.summary }}
+            </p>
+          </div>
+        </template>
+        <template #text>
+          <p>Maybe a badge or 2?</p>
+        </template>
+      </card>
+    </div>
   </section>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+import Card from '../components/Card.vue'
+
+const projects = computed(() => {
+  return [
+    {
+      name: 'Kyanite',
+      type: 'Library',
+      summary: 'A fully functional library of functions for use in any app',
+      link: 'https://github.com/dhershman1/kyanite'
+    }
+  ]
+})
+</script>
+
+<style scoped>
+.cards {
+  margin-top: 1rem;
+}
+.summary {
+  max-width: 300px;
+}
+</style>
