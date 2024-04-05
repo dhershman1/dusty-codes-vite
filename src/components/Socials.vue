6889be3c5f56fe57109628c89758d36576147ecd
dhershman1 <dustinh17@gmail.com>
2024-04-05 13:08:34 -0400

    Added: Socials component

---
+++ src/components/Socials.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="socials">
+    <a
+      href="https://twitter.com/sirdugs"
+      target="_blank"
+    >
+      <vue-feather type="twitter" />
+    </a>
+    <a
+      href="https://github.com/dhershman1"
+      target="_blank"
+    >
+      <vue-feather type="github" />
+    </a>
+    <a
+      href="https://www.linkedin.com/in/dustin-hershman/"
+      target="_blank"
+    >
+      <vue-feather type="linkedin" />
+    </a>
+    <a href="mailto:dustinh17@gmail.com">
+      <vue-feather type="mail" />
+    </a>
+  </section>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -20,31 +20,13 @@
         <p>Please take your time to checkout my skills and projects!</p>
       </article>
     </div>
-    <div class="socials">
-      <a
-        href="https://twitter.com/sirdugs"
-        target="_blank"
-      >
-        <vue-feather type="twitter" />
-      </a>
-      <a
-        href="https://github.com/dhershman1"
-        target="_blank"
-      >
-        <vue-feather type="github" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/dustin-hershman/"
-        target="_blank"
-      >
-        <vue-feather type="linkedin" />
-      </a>
-      <a href="mailto:dustinh17@gmail.com">
-        <vue-feather type="mail" />
-      </a>
-    </div>
+    <socials />
   </section>
 </template>
+
+<script setup>
+import Socials from '../components/Socials.vue'
+</script>
 
 <style scoped>
 figure {
