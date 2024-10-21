147eec38f0ed03802ac83aa06e5c6fa80041d291
dhershman1 <dustinh17@gmail.com>
2024-10-21 10:42:03 -0400

    Added: aria-labels to social links

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -3,24 +3,28 @@
     <a
       href="https://twitter.com/sirdugs"
       target="_blank"
+      aria-label="My Twitter Profile"
     >
       <vue-feather type="twitter" />
     </a>
     <a
       href="https://github.com/dhershman1"
       target="_blank"
+      aria-label="My Github Profile"
     >
       <vue-feather type="github" />
     </a>
     <a
       href="https://www.linkedin.com/in/dustin-hershman/"
       target="_blank"
+      aria-label="My LinkedIn Profile"
     >
       <vue-feather type="linkedin" />
     </a>
     <a
       href="https://bsky.app/profile/dhershman.bsky.social"
       target="_blank"
+      aria-label="My BlueSky Profile"
     >
       <vue-feather type="at-sign" />
     </a>
