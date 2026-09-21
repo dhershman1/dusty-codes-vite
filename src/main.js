c96a72dde872a19932b294ace44b2ec76044413f
dhershman1 <dustinh17@gmail.com>
2026-09-21 17:11:18 -0400

    Updated: remove feather icons usage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import VueFeather from 'vue-feather'
 import { MotionPlugin } from '@vueuse/motion'
 
 import router from './router'
@@ -15,7 +14,6 @@ import './assets/css/brand.css'
 
 const app = createApp(App)
 
-app.component(VueFeather.name, VueFeather)
 app.use(MotionPlugin)
 app.use(router)
 app.mount('#app')
